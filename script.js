b39a26b3924edc9d60bd4a69fecb87703f275e1d
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2026-02-25 09:32:55 -0300

    exercicios de javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+// let filme
+// filmes = ["Como eu era antes de voce", "10 coisas que odeio em voce", "Culpa nossa"]
+
+
+
+
+
+// filmes.forEach(function (item) {
+//     alert("filmes bom " + item)
+// })
+
+//listasCompras[0] = "Abacate"
+//pop tira no final
+//push adiciona no final
+//shift tira no inicio
+//unshift adiciona no inicio
+//.length
+
+function fazer(a){
+console.log(a+ 1)
+}
